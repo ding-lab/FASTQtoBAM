8450e494911876366374a477f35e84fbdc5bd779
yize.li <yize.li@wustl.edu>
2019-06-17 11:54:23 -0500

    FASTQtoBAM configuration file updated by the users

--- config.sh
+++ config.sh
@@ -1,10 +1,20 @@
 ## Arguments defined by users
 
-sample_ID="" # Sample name used for creating the final BAM file
-input="" # Path of getting the FASTQ files
-output="" # Path of saving the BAM files
-reference="" # The reference used for alignment
+### Sample name used for creating the final BAM file
+sample_ID=""
 Lane1_1="" # Name of 1st paired FASTQ file
 Lane1_2="" # Name of 2nd paired FASTQ file
 Lane2_1="" # If no lane 2, leave it as ""
 Lane2_2="" # If no lane 2, leave it as ""
+
+### Paths
+input="" # Path of getting the FASTQ files
+output="" # Path of saving the BAM files
+
+### Reference
+reference="" # The reference used for alignment
+
+### Tools (if work in Katmai, no need to make changes)
+flexbar="/diskmnt/Projects/Users/yize.li/Tools/conda_yize/bin/flexbar"
+bwa="/diskmnt/Projects/Users/yize.li/Tools/conda_yize/bin/bwa"
+samtools="/diskmnt/Projects/Users/yize.li/Tools/conda_yize/bin/samtools"
