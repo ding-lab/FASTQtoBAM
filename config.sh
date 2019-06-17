4ca4fa1e7af0d5f2448a034c073406bf658a2985
yize.li <yize.li@wustl.edu>
2019-06-17 11:29:22 -0500

    Configuration file updated by users

---
+++ config.sh
@@ -0,0 +1,10 @@
+## Arguments defined by users
+
+sample_ID="" # Sample name used for creating the final BAM file
+input="" # Path of getting the FASTQ files
+output="" # Path of saving the BAM files
+reference="" # The reference used for alignment
+Lane1_1="" # Name of 1st paired FASTQ file
+Lane1_2="" # Name of 2nd paired FASTQ file
+Lane2_1="" # If no lane 2, leave it as ""
+Lane2_2="" # If no lane 2, leave it as ""
