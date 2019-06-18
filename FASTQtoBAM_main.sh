b23db948fd6d6d6492dca0119823dc6dad7ac034
yize.li <yize.li@wustl.edu>
2019-06-18 11:35:41 -0500

    FASTQtoBAM main script

--- FASTQtoBAM_main.sh
+++ FASTQtoBAM_main.sh
@@ -2,7 +2,7 @@
 
 #######################################################################################################
 ################################################# Y Li ################################################
-######################## Date: 08/15/2018 (Initiation); 06/17/2019 (Revision) #########################
+######################## Date: 08/15/2018 (Initiation); 06/18/2019 (Revision) #########################
 ################ FASTQ to BAM  pipeline on Katmai (regular WES, WGS, 10X WES/WGS etc) #################
 #######################################################################################################
 
@@ -17,31 +17,60 @@ source config.sh
 # Create a temp folder which will be used in Step 5 (Picard)
 mkdir ${output}/temp
 
-# Lane 1
+# Pre-alignment steps
+## Lane 1
 if [$Lane1_1 = ""]; then
-	echo "Error: FASTQ files missing. Please double check."
+	echo "Error: FASTQ files missing (at least one lane). Please double check."
 else
 	## Step 1: trimming: barcode and adapter removal
 	${flexbar} -u 100 -n 8 -r ${input}/${Lane1_1} -t ${output}/temp/${Lane1_1} -x 23 -z GZ &
 	ln -s ${input}/${Lane1_2} ${output}/temp/${Lane1_2}.fastq.gz &
 
 	## Step 2: mapping: hg38
-	${bwa} mem -t 8 -M ${reference} ${output}/temp/${Lane1_1}.fastq.gz ${output}/temp/${Lane1_2}.fastq.gz | ${samtools} view -Sb - > ${output}/${Lane1_BAM}.bam
+	### Add Read group information: Read group information is typically added during this step, but can also be added or modified after mapping using Picard AddOrReplaceReadGroups.
+	for R1 in ${Lane1_1};do
+    		SM=$(echo $R1 | cut -d"_" -f1)                                          ##sample ID
+    		LB=$(echo $R1 | cut -d"_" -f1,2)                                        ##library ID
+    		PL="Illumina"                                                           ##platform (e.g. illumina, solid)
+    		RGID=$(zcat $R1 | head -n1 | sed 's/:/_/g' |cut -d "_" -f1,2,3,4)       ##read group identifier 
+    		PU=$RGID.$LB                                                            ##Platform Unit
+    		echo -e "@RG\tID:$RGID\tSM:$SM\tPL:$PL\tLB:$LB\tPU:$PU"
+
+    		R2=$(echo $R1 | sed 's/_R1_/_R2_/')
+    		echo $R1 $R2
+		### Mapping
+    		${bwa} mem -t 8 -M -R "@RG\tID:$RGID\tSM:$SM\tPL:$PL\tLB:$LB\tPU:$PU" ${reference} ${output}/temp/$R1 ${output}/temp/$R2 | ${samtools} view -Sb - > ${output}/${Lane1_BAM}.bam
+  	done
 fi
 
-# Lane 2 (if no lane 2, print out the note)
+## Lane 2 (if no lane 2, print out the note)
 if [$Lane2_1 = ""]; then
         echo "Note: There is only one lane for this sample."
 else
+	echo "Note: There are two lanes for this sample. Merging step (step 4) will be processed."
 	## Step 1: trimming: barcode and adapter removal
 	${flexbar} -u 100 -n 8 -r ${input}/${Lane2_1} -t ${output}/temp/${Lane2_1} -x 23 -z GZ
 	ln -s ${input}/${Lane2_2} ${output}/temp/${Lane2_2}.fastq.gz
 
-	## Step 2: mapping: hg38
-	${bwa} mem -t 8 -M ${reference} ${output}/temp/${Lane2_1}.fastq.gz ${output}/temp/${Lane2_2}.fastq.gz | ${samtools} view -Sb - > ${output}/${Lane2_BAM}.bam
+	#${bwa} mem -t 8 -M ${reference} ${output}/temp/${Lane2_1}.fastq.gz ${output}/temp/${Lane2_2}.fastq.gz | ${samtools} view -Sb - > ${output}/${Lane2_BAM}.bam
+        ## Step 2: mapping: hg38
+        ### Add Read group information
+        for R1 in ${Lane1_1};do
+                SM=$(echo $R1 | cut -d"_" -f1)                                          ##sample ID
+                LB=$(echo $R1 | cut -d"_" -f1,2)                                        ##library ID
+                PL="Illumina"                                                           ##platform (e.g. illumina, solid)
+                RGID=$(zcat $R1 | head -n1 | sed 's/:/_/g' |cut -d "_" -f1,2,3,4)       ##read group identifier
+                PU=$RGID.$LB                                                            ##Platform Unit
+                echo -e "@RG\tID:$RGID\tSM:$SM\tPL:$PL\tLB:$LB\tPU:$PU"
+
+                R2=$(echo $R1 | sed 's/_R1_/_R2_/')
+                echo $R1 $R2
+                ### Mapping
+                ${bwa} mem -t 8 -M -R "@RG\tID:$RGID\tSM:$SM\tPL:$PL\tLB:$LB\tPU:$PU" ${reference} ${output}/temp/$R1 ${output}/temp/$R2 | ${samtools} view -Sb - > ${output}/${Lane1_BAM}.bam
+        done
 fi
 
-
+# Post-alignment steps
 if [$Lane2_1 = ""]; then
 	## Step 3: Sorting
 	for i in ${Lane1_BAM}
@@ -50,9 +79,13 @@ if [$Lane2_1 = ""]; then
 	done
 
 	## Step 4: Merging (if no lane 2, no need to do merging)
+	## Skipped
+
+	## Optional: Add read group here
+	#java -jar ${picard} AddOrReplaceReadGroups I=${output}/${sample_ID}.sorted.bam O=${output}/${sample_ID}.sorted.addRG.bam RGID=${sample_ID} RGLB=lib1 RGPL=illumina RGPU=unit1 RGSM=${sample_ID}
 
 	## Step 5: Removing duplicates
-	java -jar -Djava.io.tmpdir=${output}/temp ${picard} AddOrReplaceReadGroups MarkDuplicates I=${output}/${sample_ID}.sorted.bam O=${output}/${sample_ID}.sorted.marked_duplicates.bam M=${output}/marked_dup_metrics.txt REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=STRICT
+	java -jar -Djava.io.tmpdir=${output}/temp ${picard} MarkDuplicates I=${output}/${sample_ID}.sorted.bam O=${output}/${sample_ID}.sorted.marked_duplicates.bam M=${output}/marked_dup_metrics.txt REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=STRICT
 
 	## Step 6: Indexing
 	${samtools} index ${output}/${sample_ID}.sorted.marked_duplicates.bam
@@ -67,7 +100,7 @@ else
 	${samtools} merge ${output}/${sample_ID}.sorted.bam ${output}/${Lane1_BAM}.sorted.bam ${output}/${Lane2_BAM}.sorted.bam
 
 	## Step 5: Removing duplicates
-	java -jar -Djava.io.tmpdir=${output}/temp ${picard} AddOrReplaceReadGroups MarkDuplicates I=${output}/${sample_ID}.sorted.bam O=${output}/${sample_ID}.sorted.marked_duplicates.bam M=${output}/marked_dup_metrics.txt REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=STRICT
+	java -jar -Djava.io.tmpdir=${output}/temp ${picard} MarkDuplicates I=${output}/${sample_ID}.sorted.bam O=${output}/${sample_ID}.sorted.marked_duplicates.bam M=${output}/marked_dup_metrics.txt REMOVE_DUPLICATES=true VALIDATION_STRINGENCY=STRICT
 
 	## Step 6: Indexing
 	${samtools} index ${output}/${sample_ID}.sorted.marked_duplicates.bam
