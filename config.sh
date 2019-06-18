cf4942f5145b1f51cabb0bc25fa97409507f35df
yize.li <yize.li@wustl.edu>
2019-06-18 12:01:05 -0500

    FASTQtoBAM configuration file updated by the users

--- config.sh
+++ config.sh
@@ -1,18 +1,18 @@
 ## Arguments defined by users
 
 ### Sample name used for creating the final BAM file
-sample_ID=""
-Lane1_1="" # Name of 1st paired FASTQ file
-Lane1_2="" # Name of 2nd paired FASTQ file
-Lane2_1="" # If no lane 2, leave it as ""
-Lane2_2="" # If no lane 2, leave it as ""
+sample_ID="" # e.g. TWAW-K1600481-cyst_5PCRga
+Lane1_1="" # Name of 1st paired FASTQ file e.g. TWAW-K1600481-cyst_5PCRga_S1_L003_1.fastq.gz
+Lane1_2="" # Name of 2nd paired FASTQ file e.g. TWAW-K1600481-cyst_5PCRga_S1_L003_2.fastq.gz
+Lane2_1="" # If no lane 2, leave it as ""; if yes, add the name e.g. K1600481-cyst_5_L004_1.fastq.gz
+Lane2_2="" # If no lane 2, leave it as ""; if yes, add the name e.g. K1600481-cyst_5_L004_2.fastq.gz
 
 ### Paths
 input="" # Path of getting the FASTQ files
 output="" # Path of saving the BAM files
 
 ### Reference
-reference="" # The reference used for alignment
+reference="" # The reference used for alignment, could be either hg19 or hg38
 
 ### Tools (if work in Katmai, no need to make changes)
 flexbar="/diskmnt/Projects/Users/yize.li/Tools/conda_yize/bin/flexbar"
